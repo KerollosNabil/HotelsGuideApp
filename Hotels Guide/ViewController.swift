94ece8409a9473f07c2716bc0625502f39667b48
KerollosNabil <kokonabil4444@gmail.com>
2021-01-21 19:29:46 +0200

    Initial Commit

---
+++ Hotels Guide/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hotels Guide
+//
+//  Created by MAC on 21/01/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
