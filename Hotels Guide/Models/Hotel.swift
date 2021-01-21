67c49746e9ebe5f23d66ff1974bc677f8c7f4bd3
KerollosNabil <kokonabil4444@gmail.com>
2021-01-22 01:56:42 +0200

    add hotels model

--- Hotels Guide/Models/Hotel.swift
+++ Hotels Guide/Models/Hotel.swift
@@ -6,3 +6,41 @@
 //
 
 import Foundation
+
+struct Hotels:Codable {
+
+    let hotels:[Hotel]
+    enum CodingKeys: String, CodingKey {
+        case hotels = "hotel"
+    }
+}
+
+struct Hotel:Codable {
+
+    let hotelId:Int
+    let images:[Image]
+    let location:HotelLocation
+    let hotelSummary:HotelSummary
+    
+    enum CodingKeys: String, CodingKey {
+        case hotelId
+        case images = "image"
+        case location
+        case hotelSummary = "summary"
+    }
+    
+    
+}
+struct Image:Codable {
+    let url:String
+}
+struct HotelLocation:Codable {
+    let address:String
+    let latitude:Double
+    let longitude:Double
+}
+struct HotelSummary:Codable {
+    let highRate:Double
+    let lowRate:Double
+    let hotelName:String
+}
